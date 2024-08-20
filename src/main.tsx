1f4b6eb4c4eb9f6d7d06bb683e24b759be3c70ea
AyoungWon <wayinwayne@gmail.com>
2024-08-20 18:34:29 +0900

    chore: Jotai, ReactQuery, axios 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
+
+// QueryClient 생성
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
