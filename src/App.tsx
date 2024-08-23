4689c3509bffd7a5c34a92d258897e26155c8964
AyoungWon <wayinwayne@gmail.com>
2024-08-24 00:16:39 +0900

    style: 레이아웃 컴포넌트 설정전 임시 레이아웃 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,82 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "pages/Home";
 import Menu from "pages/Menu";
 import MenuDetail from "pages/MenuDetail";
 import Profile from "pages/Profile";
 
+//아래 Route외 html Layout 컴포넌트로 변경 예정
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home role="store-owner" />} />
-      <Route path="/menu" element={<Menu />} />
-      {/* 관리자만 */}
-      <Route path="/menu/:id" element={<MenuDetail />} />
-      <Route path="/order" element={<Menu />} />
-      {/* 관리자만 */}
-      <Route path="/order/:id" element={<MenuDetail />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
+    <div css={_container}>
+      <aside css={_sideMenu}>
+        <h1>
+          <Link to="/" aria-label="Home">
+            LOGO 자리
+          </Link>
+        </h1>
+        <nav aria-label="Sidebar navigation">
+          <ul>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="order">Order</Link>
+            </li>
+            <li>
+              <Link to="menu">Menu</Link>
+            </li>
+            <li>
+              <Link to="review">Review</Link>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+      <div css={_wrapper}>
+        <header css={_header}>
+          <div>
+            <form role="search">
+              <label htmlFor="search">Search</label>
+              <input id="search" type="search" placeholder="Search..." />
+            </form>
+          </div>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home role="store-owner" />} />
+            <Route path="/menu" element={<Menu />} />
+            {/* 관리자만 */}
+            <Route path="/menu/:id" element={<MenuDetail />} />
+            <Route path="/order" element={<Menu />} />
+            {/* 관리자만 */}
+            <Route path="/order/:id" element={<MenuDetail />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </main>
+      </div>
+    </div>
   );
 }
 
 export default App;
+
+const _container = css`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+`;
+
+const _sideMenu = css`
+  width: 345px;
+  background-color: #e8e8e8;
+`;
+
+const _wrapper = css`
+  flex: 1;
+`;
+
+const _header = css`
+  height: 120px;
+  background-color: aliceblue;
+`;
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./styles/global.css";
-import App from "App";
+import App from "app";
 
 // QueryClient 생성
 const queryClient = new QueryClient();
