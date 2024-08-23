0ab2a42cd8534cdb834f59539352e353febb74a8
jangwon <yoonjangwon94@gmail.com>
2024-08-23 12:56:45 +0900

    feat: axios basic core instance

---
+++ src/api/core/index.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const BASE_URL = "/api/v1/"; // process.env.REACT_APP_API_BASE_URL; 추후
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    "Cache-Control": "no-cache, no-store, must-revalidate",
+  },
+});
+
+// 요청 인터셉터
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    // 요청 보내기 전에 수행할 로직
+    if (!config.headers) {
+      return Promise.reject(new Error("헤더 정보가 없습니다."));
+    }
+
+    return config;
+  },
+
+  function (error) {
+    console.error(error); //  디버깅
+    return Promise.reject(new Error(error));
+  }
+);
+
+// 응답 인터셉터
+axiosInstance.interceptors.response.use(
+  function (response) {
+    // 응답 데이터
+    return response;
+  },
+  async function (error) {
+    // 응답 에러
+    console.error(error); // 디버깅
+
+    return Promise.reject(new Error(error));
+  }
+);
+
+export default axiosInstance;
