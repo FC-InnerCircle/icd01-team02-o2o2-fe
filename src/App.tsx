418355f23d07489b586077fd85b55b31b25e2852
AyoungWon <wayinwayne@gmail.com>
2024-08-23 23:41:16 +0900

    feat: 기능별 페이지 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Menu from "pages/Menu";
+import MenuDetail from "pages/MenuDetail";
+import Profile from "pages/Profile";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home role="store-owner" />} />
+      <Route path="/menu" element={<Menu />} />
+      {/* 관리자만 */}
+      <Route path="/menu/:id" element={<MenuDetail />} />
+      <Route path="/order" element={<Menu />} />
+      {/* 관리자만 */}
+      <Route path="/order/:id" element={<MenuDetail />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 }
