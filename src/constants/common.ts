e5cc797f3cc00b22ef2e581db6d498e0007490f8
AyoungWon <wayinwayne@gmail.com>
2024-08-24 17:40:37 +0900

    refactor: 역할별 접근가능 페이지 관리를 위해 페이지별 Path 변경

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,10 +1,14 @@
 export const ROUTES = {
   HOME: "/",
-  ORDER: "/order",
-  ORDER_DETAIL: "/order/:id",
-  MENU: "/menu",
-  MENU_DETAIL: "/menu/:id",
-  REVIEW: "/review",
+  STORE: "/store", // 스토어 리스트, 관리자만 접근
+  STORE_DETAIL: "/store/:storeId",
+  USER: "/user", // 유저 리스트, 관리자만 접근
+  USER_DETAIL: "user/:userId", //관리자만 접근
+  ORDER: "/:storeId/order",
+  ORDER_DETAIL: "/:storeId/order/:orderId",
+  MENU: "/:storeId/menu",
+  MENU_DETAIL: "/:storeId/menu/:menuId",
+  REVIEW: "/:storeId/review",
   PROFILE: "/profile",
   SIGN_IN: "/sign-in",
 } as const;
