f1f0daa34fb78cd00340750e0652d662c755313b
AyoungWon <wayinwayne@gmail.com>
2024-08-24 10:21:24 +0900

    refactor: path관련 ROUTES 상수로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import { Routes, Route, Link } from "react-router-dom";
+import { ROUTES } from "constants/common";
+
 import Home from "pages/Home";
 import Menu from "pages/Menu";
 import MenuDetail from "pages/MenuDetail";
@@ -8,6 +10,7 @@ import Profile from "pages/Profile";
 import Order from "pages/Order";
 import OrderDetail from "pages/OrderDetail";
 import SignIn from "pages/SignIn";
+import Reviews from "pages/Reviews";
 
 //아래 Route외 html Layout 컴포넌트로 변경 예정
 function App() {
@@ -22,16 +25,16 @@ function App() {
         <nav aria-label="Sidebar navigation">
           <ul>
             <li>
-              <Link to="/dashboard">Dashboard</Link>
+              <Link to={ROUTES.HOME}>Dashboard</Link>
             </li>
             <li>
-              <Link to="order">Order</Link>
+              <Link to={ROUTES.ORDER}>Order</Link>
             </li>
             <li>
-              <Link to="menu">Menu</Link>
+              <Link to={ROUTES.MENU}>Menu</Link>
             </li>
             <li>
-              <Link to="review">Review</Link>
+              <Link to={ROUTES.REVIEW}>Review</Link>
             </li>
           </ul>
         </nav>
@@ -47,13 +50,14 @@ function App() {
         </header>
         <main>
           <Routes>
-            <Route path="/" element={<Home role="store-owner" />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/menu/:id" element={<MenuDetail />} />
-            <Route path="/order" element={<Order />} />
-            <Route path="/order/:id" element={<OrderDetail />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/sign-in" element={<SignIn />} />
+            <Route path={ROUTES.HOME} element={<Home role="store-owner" />} />
+            <Route path={ROUTES.MENU} element={<Menu />} />
+            <Route path={ROUTES.MENU_DETAIL} element={<MenuDetail />} />
+            <Route path={ROUTES.ORDER} element={<Order />} />
+            <Route path={ROUTES.ORDER_DETAIL} element={<OrderDetail />} />
+            <Route path={ROUTES.REVIEW} element={<Reviews />} />
+            <Route path={ROUTES.PROFILE} element={<Profile />} />
+            <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
           </Routes>
         </main>
       </div>
---
+++ src/constants/common.ts
@@ -0,0 +1,10 @@
+export const ROUTES = {
+  HOME: "/",
+  ORDER: "/order",
+  ORDER_DETAIL: "/order/:id",
+  MENU: "/menu",
+  MENU_DETAIL: "/menu/:id",
+  REVIEW: "/review",
+  PROFILE: "/profile",
+  SIGN_IN: "/sign-in",
+} as const;
