4d89108a5f168a22c2e80cde5b2632bdddb00077
AyoungWon <wayinwayne@gmail.com>
2024-08-24 14:27:14 +0900

    style: color 팔레트 추가

---
+++ src/styles/color.ts
@@ -0,0 +1,16 @@
+const colors = {
+  primary: "#2F4CDD", // Primary
+  secondary: "#DEE4FF", // Secondary
+  accent: "#B519EC", // Accent
+  textPrimary: "#000000", // Text Primary
+  textSecondary: "#0F0F0F", // Text Secondary
+  textMuted: "#3E4954", // Text Muted
+  backgroundLight: "#F5F6FA", // Background Light
+  success: "#28C76F", // Success
+  danger: "#EA5455", // Danger
+  warning: "#FF9F43", // Warning
+  darkBackground: "#2C2E33", // Dark Background
+  brightOrange: "#FF6F61", // Bright Orange
+};
+
+export default colors;
