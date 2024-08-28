aaa25cb3a5adc0108d7eaccac7dfe42a4e48ee6a
AyoungWon <wayinwayne@gmail.com>
2024-08-29 07:35:05 +0900

    feat: 메뉴 아이콘들 추가

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,5 +1,9 @@
-import Search from "./search.svg";
+import Search from "./assets/search.svg";
+import Dashboard from "./assets/dashboard.svg";
+import Order from "./assets/order.svg";
+import Menu from "./assets/menus.svg";
+import Review from "./assets/review.svg";
 
-const Icons = { Search };
+const Icons = { Search, Dashboard, Order, Menu, Review };
 
 export { Icons as default };
