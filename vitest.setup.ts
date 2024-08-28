e13dd4ba8828c722cd457fb9ed4546ffb99d1310
jangwonyoon <wkddnjs7310@naver.com>
2024-08-28 11:41:00 +0900

    test: vitest msw 테스트 환경 셋업

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -2,6 +2,8 @@
 import { beforeAll, afterEach, afterAll } from 'vitest'
 import { server } from './src/mocks/server';
 
+import '@testing-library/jest-dom';
+
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
