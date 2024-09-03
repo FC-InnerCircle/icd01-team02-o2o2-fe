626aaf15a19e95c07de450660f2552a7e656ba18
AyoungWon <wayinwayne@gmail.com>
2024-09-03 15:55:32 +0900

    feat: trash, edit icon 추가

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -3,6 +3,9 @@ import Dashboard from "./assets/dashboard.svg";
 import Order from "./assets/order.svg";
 import Menu from "./assets/menus.svg";
 import Review from "./assets/review.svg";
+/* 위에도 추후 아래처럼 변경 예정 */
+export { default as EditIcon } from "./assets/edit.svg";
+export { default as DeleteIcon } from "./assets/trash.svg";
 
 const Icons = { Search, Dashboard, Order, Menu, Review };
 
