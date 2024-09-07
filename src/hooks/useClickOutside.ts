e6401fc3dcf70fff337fef7b50826f885b6956b0
AyoungWon <wayinwayne@gmail.com>
2024-09-07 10:09:53 +0900

    docs: useClickOutside 훅에 Jsdoc 추가

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -1,5 +1,22 @@
 import { useEffect, useRef } from "react";
 
+/**
+ * 지정된 요소 밖에서 클릭 또는 터치 이벤트가 발생하면 콜백을 실행하는 커스텀 훅입니다.
+ *
+ * @template T - ref가 적용될 HTMLElement의 타입입니다.
+ * @param {() => void} onClickOutside - 참조된 요소 밖에서 클릭 또는 터치 이벤트가 발생할 때 실행되는 콜백 함수입니다.
+ * @returns {{ ref: React.RefObject<T> }} `ref`를 포함한 객체를 반환하며, 이 ref는 타겟 요소에 연결되어야 합니다.
+ *
+ * @example
+ * const Component = () => {
+ *   const handleClickOutside = () => {
+ *     console.log('요소 밖을 클릭했습니다.');
+ *   };
+ *   const { ref } = useClickOutside(handleClickOutside);
+ *
+ *   return <div ref={ref}>내용</div>;
+ * };
+ */
 const useClickOutside = <T extends HTMLElement>(onClickOutside: () => void) => {
   const ref = useRef<T>(null);
   useEffect(() => {
