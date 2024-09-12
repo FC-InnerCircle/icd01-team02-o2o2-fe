0c5d52312f471ed5e9a6358378f64d5ec0f2da02
AyoungWon <wayinwayne@gmail.com>
2024-09-12 15:49:44 +0900

    feat: plus icon 추가

--- src/common/components/icons/index.ts
+++ src/common/components/icons/index.ts
@@ -3,6 +3,6 @@ export { default as Dashboard } from "./assets/dashboard.svg";
 export { default as Order } from "./assets/order.svg";
 export { default as Menu } from "./assets/menus.svg";
 export { default as Review } from "./assets/review.svg";
-
 export { default as Edit } from "./assets/edit.svg";
 export { default as Delete } from "./assets/trash.svg";
+export { default as Plus } from "./assets/plus.svg";
