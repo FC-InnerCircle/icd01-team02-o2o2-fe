3bf46163a302d7bc43173e4150ec8d4c089ba533
AyoungWon <wayinwayne@gmail.com>
2024-09-30 18:57:18 +0900

    chore: cypress 컴포넌트 테스트를 src/comoon/cypress, cypress/components 둘다 되도록 설정

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,7 +7,10 @@ export default defineConfig({
       bundler: 'vite',
     },
     // 명시적으로 Cypress가 컴포넌트 테스트 파일을 찾을 수 있는 경로를 설정
-    specPattern: 'src/common/components/**/*.cy.{js,jsx,ts,tsx}',
+    specPattern: [
+      'cypress/components/**/*.cy.{js,jsx,ts,tsx}', // cypress/components 경로 내 테스트 파일
+      'src/common/components/**/*.cy.{js,jsx,ts,tsx}', // src/common/components 경로 내 테스트 파일
+    ],
 
     // Cypress가 자동으로 명령어를 불러오도록 설정
     supportFile: 'cypress/support/commands.ts',
