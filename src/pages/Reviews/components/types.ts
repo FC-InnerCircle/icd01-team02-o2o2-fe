7264c02e4c39929f71e6c5c842e3d413f1fb3405
Seung-wan <tmddhks0104@gmail.com>
2024-10-05 10:35:42 +0900

    fix: fix ignorecase

--- src/pages/Reviews/components/types.ts
+++
@@ -1,8 +0,0 @@
-import { type Review } from 'api/modules/stores/types';
-
-export type CardProps = {} & Omit<Review, 'id'>;
-
-export type StarRatingProps = {
-  rating: number;
-  size?: number;
-};
