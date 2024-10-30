90b8b1c5a51e8d3b1c29377ad62289f59f389049
AyoungWon <wayinwayne@gmail.com>
2024-10-30 19:20:04 +0900

    feat: routes에 analytics 추가

--- src/common/constants/routes.ts
+++ src/common/constants/routes.ts
@@ -1,17 +1,18 @@
 export const ROUTES = {
-  HOME: "/",
-  STORE: "/store", // 스토어 리스트, 관리자만 접근
-  STORE_DETAIL: "/store/:storeId",
-  USER: "/user", // 유저 리스트, 관리자만 접근
-  USER_DETAIL: "user/:userId", //관리자만 접근
-  ORDER: "/:storeId/order",
-  ORDER_DETAIL: "/:storeId/order/:orderId",
-  MENU: "/:storeId/menu",
-  MENU_REGISTRATION: "/:storeId/menu/registration",
-  MENU_DETAIL: "/:storeId/menu/:menuId",
-  REVIEW: "/:storeId/review",
-  PROFILE: "/profile",
-  SIGN_IN: "/sign-in",
+  HOME: '/',
+  STORE: '/store', // 스토어 리스트, 관리자만 접근
+  STORE_DETAIL: '/store/:storeId',
+  USER: '/user', // 유저 리스트, 관리자만 접근
+  USER_DETAIL: 'user/:userId', //관리자만 접근
+  ORDER: '/:storeId/order',
+  ORDER_DETAIL: '/:storeId/order/:orderId',
+  MENU: '/:storeId/menu',
+  MENU_REGISTRATION: '/:storeId/menu/registration',
+  MENU_DETAIL: '/:storeId/menu/:menuId',
+  REVIEW: '/:storeId/review',
+  PROFILE: '/profile',
+  SIGN_IN: '/sign-in',
+  ANALYTICS: '/analytics',
 } as const;
 
-export const BASE_URL = "/api/v1/";
+export const BASE_URL = '/api/v1/';
